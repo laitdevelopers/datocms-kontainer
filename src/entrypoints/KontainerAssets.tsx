19b40c2ae107ad0993a33cb01e493bfcf7be1901
Søren <soeren@lait.dk>
2022-09-15 10:13:46 +0200

    UNI-255: Allow multiple.

--- src/entrypoints/KontainerAssets.tsx
+++ src/entrypoints/KontainerAssets.tsx
@@ -1,5 +1,6 @@
-import { RenderFieldExtensionCtx } from "datocms-plugin-sdk";
+import { IntentCtx, RenderFieldExtensionCtx } from "datocms-plugin-sdk";
 import { Button, Canvas } from "datocms-react-ui";
+import { arrayBuffer } from "stream/consumers";
 
 type PropTypes = {
 	ctx: RenderFieldExtensionCtx;
@@ -8,7 +9,7 @@ type PropTypes = {
 const KontainerAssets = ({ ctx }: PropTypes) => {
 	let isOpen = false;
 	let popUpUrl: string;
-
+	let multiSelect = ctx.parameters["multiSelect"] === true;
 	let kontainerDomain = ctx.plugin.attributes.parameters["domain"] as string;
 	if (kontainerDomain == null) return null;
 
@@ -24,29 +25,28 @@ const KontainerAssets = ({ ctx }: PropTypes) => {
 		.replace("http://", "https://")
 		.replace(/(\.\w+)\/.*$/, "$1");
 
-	let currentValue = JSON.parse(
+	let assets = JSON.parse(
 		ctx.formValues[ctx.fieldPath] as string
-	) as KontainerEventData;
+	) as KontainerEventData[];
+	if (!Array.isArray(assets)) assets = [];
 
-	const edit = () => {
+	const edit = (asset?: KontainerEventData) => {
 		isOpen = true;
 
 		const eventListener = (event: MessageEvent<string>) => {
-			if (event.data == null) {
+			if (event.data == null || typeof event.data !== "string") {
 				throw Error("No data was recieved from Kontainer.");
 			}
-
 			const content = JSON.parse(event.data) as KontainerEventData;
+			updateItem(content, asset);
 			console.log(content);
-			ctx.setFieldValue(ctx.fieldPath, event.data);
-			currentValue = ctx.formValues[ctx.fieldPath] as KontainerEventData;
 		};
 
 		window.addEventListener("message", eventListener, { once: true });
 
 		const url = `${popUpUrl}?cmsMode=1`;
+		console.log(url);
 		const popup = window.open(url);
-
 		const interval = window.setInterval(() => {
 			if (popup?.closed) {
 				window.clearInterval(interval);
@@ -61,15 +61,45 @@ const KontainerAssets = ({ ctx }: PropTypes) => {
 		ctx.setFieldValue(ctx.fieldPath, null);
 	};
 
+	const updateItem = (
+		newItem: KontainerEventData,
+		oldItem?: KontainerEventData
+	) => {
+		let currentAssets = ctx.formValues[
+			ctx.fieldPath
+		] as KontainerEventData[];
+		if (!Array.isArray(currentAssets)) currentAssets = [];
+		if (oldItem != null) {
+			const index = currentAssets.indexOf(oldItem);
+			currentAssets[index] = newItem;
+		} else {
+			currentAssets.push(newItem);
+		}
+		ctx.setFieldValue(ctx.fieldPath, JSON.stringify(currentAssets));
+
+		isOpen = false;
+	};
+
 	return (
 		<Canvas ctx={ctx}>
-			<img
-				style={{ height: "100px", width: "100px", cursor: "pointer" }}
-				onClick={edit}
-				src={currentValue?.url}
-				alt={currentValue?.alt ?? undefined}
-			/>
-			<Button onClick={remove}>Remove</Button>
+			<div>
+				{assets.map((asset, index) => (
+					<template key={index}>
+						<img
+							style={{
+								height: "100px",
+								width: "100px",
+								cursor: "pointer",
+							}}
+							onClick={() => {edit()}}
+							src={asset?.url}
+							alt={asset?.alt ?? undefined}
+						/>
+						<Button onClick={remove}>Remove</Button>
+					</template>
+				))}
+				<Button onClick={() => {edit()}}>Add</Button>
+			</div>
 		</Canvas>
 	);
 };
